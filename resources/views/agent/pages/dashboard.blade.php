aecc1a8ead86cfbf170c50f6d4954d9d3f64ebce
Ahmed Tamim <ahmedtamim19050@gmail.com>
2025-04-17 16:24:01 +0600

    issue

--- resources/views/agent/pages/dashboard.blade.php
+++ resources/views/agent/pages/dashboard.blade.php
@@ -1,8 +1,23 @@
 <x-agent.app>
     <div class="container padding-xxl">
-        <h4>Welcome Back Jannie</h4>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit, molestias.</p>
+        <h4 class="greading">Welcome Back Jannie !</h4>
+        <p class="greading-pera">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit, molestias.</p>
 
-
+         <div class="row">
+            <div class="col-md-3">
+                <div class="card widget">
+                     <div class="d-flex align-items-center">
+                        <div class="icon-section">
+                            <i class="material-symbols-rounded opacity-5">apartment</i>
+                        </div>
+                        <p>Total Propertices</p>
+                    </div>
+                    <div class="d-flex">
+                        <h1 class="widget-total">2,854</h1>
+                        <p class="widget-percent">76.89%</p>
+                    </div>
+                </div>
+            </div>
+         </div>
     </div>
 </x-agent.app>
