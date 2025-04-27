e6c278283b13f799b117c5fac06ddb3079c22298
Ahmed Tamim <ahmedtamim19050@gmail.com>
2025-04-27 14:02:09 +0600

    issue

--- resources/views/agent/pages/plan_Management/planManagement.blade.php
+++ resources/views/agent/pages/plan_Management/planManagement.blade.php
@@ -98,7 +98,9 @@
                                         @csrf
                                         <div class="border border-secondary rounded-3 ">
                                             <div class="pack-header p-2">
-                                                <div class="castom-current-plan">Current Plan</div>
+                                                @if (auth()->user()->plan_id == $plan->id)
+                                                    <div class="castom-current-plan">Current Plan</div>
+                                                @endif
                                                 <div class="corner-triangle"></div>
                                                 <h6 class="pack-name-header text-primary pt-4 mt-2 ps-2">
                                                     {{ $plan->name }}
